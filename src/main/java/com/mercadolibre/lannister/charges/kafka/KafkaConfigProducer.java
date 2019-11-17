a7956a6faaed1b55d31e41f48c166eceea81aa36
Facundo Leonardelli <facundo.leonardelli@despegar.com>
2019-11-17 01:42:21 -0300

    General Changes

--- src/main/java/com/mercadolibre/lannister/charges/kafka/KafkaConfigProducer.java
+++ src/main/java/com/mercadolibre/lannister/charges/kafka/KafkaConfigProducer.java
@@ -1,10 +1,13 @@
 package com.mercadolibre.lannister.charges.kafka;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -14,14 +17,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class KafkaConfigProducer {
 
     @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+    String bootstrapServers;
+
+    @Value("${spring.kafka.properties.security.protocol}")
+    String protocol;
+
+    @Value("${spring.kafka.properties.sasl.mechanism}")
+    String mechanism;
+
+    @Value("${spring.kafka.properties.sasl.jaas.config}")
+    String jaasCfg;
 
     @Bean
     public Map<String, Object> producerConfigs() {
-
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -30,7 +43,9 @@ public class KafkaConfigProducer {
         props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 30000);
         props.put(ProducerConfig.RETRIES_CONFIG, 0);
         props.put(ProducerConfig.ACKS_CONFIG, "1");
-
+        props.put("security.protocol", protocol);
+        props.put("sasl.mechanism", mechanism);
+        props.put("sasl.jaas.config", jaasCfg);
         return props;
     }
 
